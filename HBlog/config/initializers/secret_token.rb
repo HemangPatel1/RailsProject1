2cbaddca5003501dbf9b12da92d2b4e1d0df380f
HemangPatel1 <hemang2@gmail.com>
2013-11-18 14:23:08 +0000

    can post and see it

---
+++ HBlog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HBlog::Application.config.secret_key_base = '9d2b678832c8578e68147057e1f21ea2a458505bfc65eaa3ac402c7ef07d6fc7537572707709c6e868852f794c5e9a90ca00d481a302743cce4e73efdf6ec65d'
