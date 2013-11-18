6e581a1b87cd207c08630eabe8d4aaf798222474
HemangPatel1 <hemang2@gmail.com>
2013-11-18 19:51:48 +0000

    added commenting feature

--- HBlog/config/routes.rb
+++ HBlog/config/routes.rb
@@ -1,6 +1,8 @@
 HBlog::Application.routes.draw do
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
